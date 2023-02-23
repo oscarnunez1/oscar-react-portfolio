31e24597c92435b7a25cf8de154d47abb98d8ab5
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-22 21:51:00 -0500

    import banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import { Row, Col, Container } from "react-bootstrap"
-import { ArrowR}
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerPhoto from "../assets/images/subtle-prism.svg"
 
 
 export const Banner = () => {
@@ -11,7 +12,10 @@ export const Banner = () => {
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>{`Hello! This is webdecoded`}<span className="wrap">Web Developer</span></h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, inventore vitae eligendi accusantium, numquam nam voluptas qui blanditiis id odit laudantium ad libero ipsam. Repudiandae dignissimos exercitationem minima expedita labore?</p>
-            <button onClick={() => console.log('connect')}>Let's Connect</button>
+            <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25} /></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerPhoto} alt="header-img" />
           </Col>
         </Row>
       </Container>
