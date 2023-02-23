480aa079ff75a1e77cb960baf36dc2fae53789f4
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-22 21:23:23 -0500

    build out banner function, install react bootstrap icons

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,20 @@
+import { Row, Col, Container } from "react-bootstrap"
+import { ArrowR}
+
+
 export const Banner = () => {
   return (
-    <>
-    </>
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>{`Hello! This is webdecoded`}<span className="wrap">Web Developer</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, inventore vitae eligendi accusantium, numquam nam voluptas qui blanditiis id odit laudantium ad libero ipsam. Repudiandae dignissimos exercitationem minima expedita labore?</p>
+            <button onClick={() => console.log('connect')}>Let's Connect</button>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
