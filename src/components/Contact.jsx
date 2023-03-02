8780805ff89a8c8203cd6a27c30d1c74c79720ed
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-02 13:59:32 -0500

    set up server.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,10 @@ export const Contact = () => {
     })
   }
 
+  const handleSubmit = () => {
+
+  }
+
   return (
     <section className="contact" id="connect">
       <Container>
@@ -33,7 +37,7 @@ export const Contact = () => {
           </Col>
           <Col md={6}>
             <h2>Let's Get In Touch</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <Row>
                 <Col sm={6} className="px-1">
                   <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
@@ -41,6 +45,22 @@ export const Contact = () => {
                 <Col sm={6} className="px-1">
                   <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                 </Col>
+                <Col sm={6} className="px-1">
+                  <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                </Col>
+                <Col>
+                  <textarea row="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                  <button type="submit"><span>{buttonText}</span></button>
+                </Col>
+                {
+                  status.message &&
+                  <Col>
+                    <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  </Col>
+                }
               </Row>
             </form>
           </Col>
