63837dd236e37891f39c4f90b776729533edff12
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-04 22:30:45 -0500

    add profile photo to banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react"
 import { Row, Col, Container } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
+import { Image } from "react-bootstrap"
+import profilePhoto from "../assets/images/profile-photo.png"
 
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0)
   const [isDeleting, setIsDeleting] = useState(false)
-  const toRotate = ["Oscar Nunez"]
+  const toRotate = ["Oscar Nunez."]
   const [text, setText] = useState('')
   const [delta, setDelta] = useState(50)
   const period = 2000
@@ -47,7 +49,8 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={12} xl={12}>
-            <h1>{`Hey There! I'm `}<span className="wrap">{text}</span></h1>
+            <Image src={profilePhoto} roundedCircle className="profile-photo mb-7"/>
+            <h1 className="mb-5">{`Hey There! I'm `}<span className="wrap">{text}</span></h1>
             <span className="tagline">Welcome to my Portfolio</span>
             <p>Full-stack software developer with a diverse background in architectural photography, journalism, and social media marketing. Passionate about applying creativity and technology to create innovative, user-friendly applications that solve real-world problems. Collaborative team player with excellent problem-solving abilities. Committed to staying up-to-date with the latest industry trends and continuously improving skills. Ready to bring unique perspectives and fresh ideas to new projects.</p>
             <a href={linkedInProfile} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none'}}><button className="mx-auto" onClick={() => console.log('connect')}>Let's Connect On LinkedIn<ArrowRightCircle size={30} /></button></a>
