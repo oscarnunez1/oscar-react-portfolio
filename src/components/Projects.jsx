75cade2fd8441a55e35dbb751807f8d9e645adaf
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-15 10:41:31 -0400

    remove empty tabs from projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import imageOne from '../assets/images/connectfour.png'
 import imageTwo from '../assets/images/cityscape.png'
@@ -55,41 +55,69 @@ export const Projects = () => {
             <h2>Projects</h2>
             <p>Explore my portfolio of recent web development projects, which demonstrate my proficiency in a variety of modern front-end and back-end technologies. Gain access to the full details of each project by clicking on the associated GitHub and demo buttons, where you can explore the technologies used, view live demos, and access the Github repository.
             </p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">               
-              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <Row>
-                    {
-                      projects.map((project, index) => {
-                        return (
-                          <ProjectCard
-                            key={index}
-                            {...project}
-                          />
-                        )
-                      })
-                    }
-                  </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second"></Tab.Pane>
-                <Tab.Pane eventKey="third"></Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
+            <Row>
+              {
+                projects.map((project, index) => {
+                  return (
+                    <ProjectCard
+                      key={index}
+                      {...project}
+                    />
+                  )
+                })
+              }
+            </Row>
           </Col>
         </Row>
       </Container>
     </section>
-  )
+  );
 
 }
+
+/************* Add Tabs when list surpasses 6 projects *************/
+
+// return (
+//   <section className="project" id="projects">
+//     <Container>
+//       <Row>
+//         <Col>
+//           <h2>Projects</h2>
+//           <p>Explore my portfolio of recent web development projects, which demonstrate my proficiency in a variety of modern front-end and back-end technologies. Gain access to the full details of each project by clicking on the associated GitHub and demo buttons, where you can explore the technologies used, view live demos, and access the Github repository.
+//           </p>
+//           <Tab.Container id="projects-tabs" defaultActiveKey="first">               
+//             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+//               <Nav.Item>
+//                 <Nav.Link eventKey="first">Tab 1</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="second">Tab 2</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="third">Tab 3</Nav.Link>
+//               </Nav.Item>
+//             </Nav>
+//             <Tab.Content>
+//               <Tab.Pane eventKey="first">
+//                 <Row>
+//                   {
+//                     projects.map((project, index) => {
+//                       return (
+//                         <ProjectCard
+//                           key={index}
+//                           {...project}
+//                         />
+//                       )
+//                     })
+//                   }
+//                 </Row>
+//               </Tab.Pane>
+//               <Tab.Pane eventKey="second"></Tab.Pane>
+//               <Tab.Pane eventKey="third"></Tab.Pane>
+//             </Tab.Content>
+//           </Tab.Container>
+//         </Col>
+//       </Row>
+//     </Container>
+//   </section>
+// )
